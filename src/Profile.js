49de75400812b159fb45a588d8c193629313c4e1
Shimil <shimiltharayil007@gmail.com>
2023-01-02 18:58:55 +0530

    completed

---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+function Profile() {
+  return (
+    <div>
+      <h2>Shimil Tharayil</h2>
+      <h2>Software Engineer</h2>
+      <img
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FLionel_Messi&psig=AOvVaw3HxVaIbDHm1CGr2NbZwCgx&ust=1672751065202000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIComMr5qPwCFQAAAAAdAAAAABAE"
+        alt=""
+        srcset=""
+      />
+    </div>
+  );
+}
+
+export default Profile;
